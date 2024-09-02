61e3ff5e7bd56850c6ca195782f5ecb85ef467c9
Steven Winters <mstevensoncc2007@gmail.com>
2024-09-02 12:17:41 +0800

    Loading Test

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,27 +1,33 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 
 const Layout = () => {
   const { pathname } = useLocation();
-  // const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
 
-  // const handleLoading = () => {
-  //   setLoading(false);
-  // };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1800);
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [pathname]);
 
-  // useEffect(() => {
-  //   window.scrollTo({ top: 0, behavior: "instant" });
-  //   window.addEventListener("load", handleLoading);
-  //   return () => window.removeEventListener("load", handleLoading);
-  // }, [pathname]);
   return (
     <div className={`${pathname === "/story" && "dark"}`}>
-      <>
-        <NavBar />
-        <Outlet />
-        <Footer />
-      </>
+      <NavBar />
+      {isLoading ? (
+        <>
+          <Loader />
+        </>
+      ) : (
+        <>
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
