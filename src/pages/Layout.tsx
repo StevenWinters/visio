f3001f1deff02bf555718aa7dc251e0cc019dbb2
Steven Winters <mstevensoncc2007@gmail.com>
2024-09-02 11:55:56 +0800

    Removed Loading Test

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,35 +1,27 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
-import { useEffect, useState } from "react";
-import Loader from "../components/Loader";
 
 const Layout = () => {
   const { pathname } = useLocation();
-  const [isLoading, setLoading] = useState(true);
+  // const [isLoading, setLoading] = useState(true);
 
-  const handleLoading = () => {
-    setLoading(false);
-  };
+  // const handleLoading = () => {
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "instant" });
-    window.addEventListener("load", handleLoading);
-    return () => window.removeEventListener("load", handleLoading);
-  }, [pathname]);
+  // useEffect(() => {
+  //   window.scrollTo({ top: 0, behavior: "instant" });
+  //   window.addEventListener("load", handleLoading);
+  //   return () => window.removeEventListener("load", handleLoading);
+  // }, [pathname]);
   return (
     <div className={`${pathname === "/story" && "dark"}`}>
-      <NavBar />
-      {!isLoading ? (
-        <>
-          <Outlet />
-          <Footer />
-        </>
-      ) : (
-        <>
-          <Loader />
-        </>
-      )}
+      <>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
     </div>
   );
 };
